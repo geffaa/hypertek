0b1fe6f39c702d22f97baca0c927d230c98c5fa1
Muhammad Idrees Younas <82458828+idrees538@users.noreply.github.com>
2025-10-10 17:52:43 +0500

    pushed

--- backend/Index.js
+++ backend/Index.js
@@ -28,7 +28,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Load environment variables
-dotenv.config({ path: "./Config/.env" });
+dotenv.config({ path: path.join(__dirname, "Config", ".env") });
 const app = express();
 
 
