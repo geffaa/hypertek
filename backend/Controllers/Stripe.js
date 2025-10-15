1d2ceafe386fc925c73eccd2ab2bd9bb00b06009
Hazrat 17880 <hazrat17780@gmail.com>
2025-10-15 10:22:20 +0500

    update code for stripe

--- backend/Controllers/Stripe.js
+++ backend/Controllers/Stripe.js
@@ -19,7 +19,7 @@ export const createCheckoutSession = async (req, res) => {
     }
 
     const session = await stripe.checkout.sessions.create({
-payment_method_types: ["card", "usdc","wechat_pay"],
+      payment_method_types: ["card"],
       mode: "payment",
       line_items: [
         {
