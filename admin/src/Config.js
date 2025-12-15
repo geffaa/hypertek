4a9c50371f7a042742811c520a715181c17837e4
Abdulwahab311 <wahabnadeem311@gmail.com>
2025-12-15 12:53:00 +0500

    updated

--- admin/src/Config.js
+++ admin/src/Config.js
@@ -1,5 +1,5 @@
-const Dashboard_Base_Url="https://admin-hyper-tek-game.deventiatech.com/api"
-const Image_Base_Url="https://admin-hyper-tek-game.deventiatech.com"
+const Dashboard_Base_Url="https://api-hyper-tek-games.deventiatech.com"
+const Image_Base_Url="https://api-hyper-tek-games.deventiatech.com"
 
 
 export { Dashboard_Base_Url,Image_Base_Url };
