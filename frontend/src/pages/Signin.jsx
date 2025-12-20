88d039fc51e4c2989f7bf3299382ece5763bb987
Abdulwahab311 <157164848+Abdulwahab311@users.noreply.github.com>
2025-12-20 16:32:37 +0500

    Update Signin.jsx

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -80,7 +80,7 @@ localStorage.setItem("authData", JSON.stringify(res.data));
 window.location.href = `https://admin-hyper-tek-game.deventiatech.com/${userId}`;
 // window.location.href = `http://localhost:5174/${userId}`;
       } else {
-       navigate("/edit", {
+       navigate("/profile", {
   state: {
     userData: res.data.user,
   },
@@ -418,3 +418,4 @@ window.location.href = `https://admin-hyper-tek-game.deventiatech.com/${userId}`
 }
 
 export default Login;
+
