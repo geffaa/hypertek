2c8a4036ad032c754776763572f32fc938f2caaa
Abdulwahab311 <wahabnadeem311@gmail.com>
2025-12-20 10:44:09 +0500

    updated

--- frontend/src/Components/ProfileSection/Land.jsx
+++ frontend/src/Components/ProfileSection/Land.jsx
@@ -177,7 +177,7 @@ function Land() {
                       </Link>
                     </p>
                     <p className="text-green-400 font-semibold mt-1 text-sm sm:text-base md:text-lg">
-                      $3000
+                    
                     </p>
                   </div>
                 </div>
