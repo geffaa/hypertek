d2fa3ba4fe783efd9edc8dc8976bc31a26201997
Abdulwahab311 <157164848+Abdulwahab311@users.noreply.github.com>
2026-01-01 15:04:00 +0500

    Adjust button text size and tracking
    Updated text size and tracking for the button.

--- frontend/src/Components/Buttons/Button1.jsx
+++ frontend/src/Components/Buttons/Button1.jsx
@@ -30,8 +30,7 @@ const CustomButton = ({ text }) => {
         className="
     flex items-center justify-center 
     text-white font-inter font-medium 
-    text-[14px] md:text-[15px]
-    leading-[1] text-center
+    text-[16px] leading-[1] tracking-[0] text-center capitalize
     md:w-[160px] w-[120px]
     py-[6px] md:py-[8px]        /* vertical padding added */
     transition-all duration-300 ease-in-out
@@ -61,3 +60,4 @@ const CustomButton = ({ text }) => {
 };
 
 export default CustomButton;
+
