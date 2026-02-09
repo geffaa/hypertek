ef020dbb7e1501cce319f7b18671049d52c9a512
Abdulwahab311 <157164848+Abdulwahab311@users.noreply.github.com>
2026-02-09 16:35:45 +0500

    Update Dashboard.jsx

--- admin/src/pages/Dashboard.jsx
+++ admin/src/pages/Dashboard.jsx
@@ -6,12 +6,12 @@ import GlowingOrb from "../components/common/BgEffect";
 const Dashboard = () => {
   return (
     <> 
-      <GlowingOrb Xaxis={120} Yaxis={50} />
+      {/* <GlowingOrb Xaxis={120} Yaxis={50} />
       <GlowingOrb Xaxis={300} Yaxis={150} />
       <GlowingOrb Xaxis={730} Yaxis={340} />
       <GlowingOrb Xaxis={20} Yaxis={630} />
       <GlowingOrb Xaxis={730} Yaxis={1040} />
-      <GlowingOrb Xaxis={60} Yaxis={1040} />
+      <GlowingOrb Xaxis={60} Yaxis={1040} /> */}
 
       <div className="flex-1 min-h-screen bg-black text-white flex justify-center overflow-x-hidden">
   <div className="w-full max-w-[1400px] px-6 mt-10">
@@ -26,3 +26,4 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
