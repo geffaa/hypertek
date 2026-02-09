bc2789a50757c1b4ed9d5536b5ce2a5e69164890
Abdulwahab311 <wahabnadeem311@gmail.com>
2026-02-09 15:55:40 +0500

    upated

--- admin/src/components/common/sidebar.jsx
+++ admin/src/components/common/sidebar.jsx
@@ -116,7 +116,11 @@ const Sidebar = ({ onLogoutClick }) => {
         {/* LOGO */}
         <div className="hidden lg:flex items-center gap-2 mt-4 ml-16">
           <img src={Logo} alt="logo" className="w-[25px]" />
-          <span className="font-bold text-lg">HYPER TEK</span>
+          <Link to="https://hyper-tek-games.deventiatech.com">
+        <span className="w-[101px] h-[22px] font-inter font-bold text-[18px] leading-[22px]">
+          HYPER TEK
+        </span>
+        </Link>
         </div>
 
         {/* MENU */}
--- frontend/src/Components/home/MarketPlace.jsx
+++ frontend/src/Components/home/MarketPlace.jsx
@@ -111,12 +111,7 @@ function PopularCollections() {
                 </div>
               </div>
 
-              {/* Button */}
-              <div className="flex justify-center items-center mt-8">
-                <Link to="/market-place">
-                  <CustomButton4 text="Buy Now" />
-                </Link>
-              </div>
+              
             </div>
           ))}
         </div>
