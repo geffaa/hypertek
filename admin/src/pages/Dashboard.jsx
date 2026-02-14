bd456a7c38a7e91e831361d7a84b6906c3efbe3f
qanitayounas <qanitayounas973@gmail.com>
2026-02-14 17:30:53 +0500

    admin panel done

--- admin/src/pages/Dashboard.jsx
+++ admin/src/pages/Dashboard.jsx
@@ -1,28 +1,25 @@
 import Graph from "../components/Dashboard/BarGraph";
 import LineGraph from "../components/Dashboard/LineGraph";
 import MarketOverview from "../components/Dashboard/MarketOverview";
-import GlowingOrb from "../components/common/BgEffect";
+import BgEffect2 from "../components/common/BgEffect2";
 
 const Dashboard = () => {
   return (
-    <> 
-      {/* <GlowingOrb Xaxis={120} Yaxis={50} />
-      <GlowingOrb Xaxis={300} Yaxis={150} />
-      <GlowingOrb Xaxis={730} Yaxis={340} />
-      <GlowingOrb Xaxis={20} Yaxis={630} />
-      <GlowingOrb Xaxis={730} Yaxis={1040} />
-      <GlowingOrb Xaxis={60} Yaxis={1040} /> */}
-<div className="flex-1 min-h-screen bg-gradient-to-b from-black to-[#002AA8] text-white flex justify-center overflow-x-hidden">
-  <div className="w-full max-w-[1400px] px-6 mt-10">
-    
-    <Graph />
-    <LineGraph />
-    <MarketOverview />
-  </div>
-</div>
-    </>
+    <div className="flex flex-col min-h-screen bg-black overflow-hidden relative text-white">
+
+    {/* Background Glowing Effects */}
+<BgEffect2 Xaxis={950} Yaxis={30} />
+
+      {/* Content */}
+      <div className="relative z-50 w-full max-w-[1400px] mx-auto px-8 mt-16">
+        <Graph />
+        <LineGraph />
+        <MarketOverview />
+      </div>
+    </div>
   );
 };
+
 
 export default Dashboard;
 
