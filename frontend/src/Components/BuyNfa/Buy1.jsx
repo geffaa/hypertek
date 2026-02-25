084ad24cda06f6517a4cb0244623f97c05c1f151
Abdulwahab311 <157164848+Abdulwahab311@users.noreply.github.com>
2026-02-25 15:50:27 +0500

    Update Buy1.jsx

--- frontend/src/Components/BuyNfa/Buy1.jsx
+++ frontend/src/Components/BuyNfa/Buy1.jsx
@@ -795,10 +795,12 @@ function Buy1() {
         setLoading(false);
 
         const targetCategory = (collection.category || collection.parentCategory || item?.category || item?.parentCategory || "characters").toLowerCase().trim();
-       navigate("/Profile", { state: { category: targetCategory } });
+        
+          navigate("/Profile", { state: { category: targetCategory } });
+        
 
-       return; // Scenario 1 completed
-
+        return; // Scenario 1 completed
+      }
       /* ================= SCENARIO 2: ALREADY MINTED ================= */
       toast.loading("🔍 Checking NFT ownership...", { id: toastId });
 
@@ -1556,4 +1558,5 @@ function Buy1() {
 
 
 export default Buy1;
+
 
