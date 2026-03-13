ec5e707df0609614fd8257ccec7930e3e3af7458
geffaa <yodhimasgeffananda@mail.ugm.ac.id>
2026-03-14 01:17:30 +0700

    fix: restore import and complete getImageUrl

--- frontend/src/pages/NewsAll.jsx
+++ frontend/src/pages/NewsAll.jsx
@@ -54,7 +54,7 @@ export default function NewsList() {
             {/* Image */}
             <div className="relative w-full sm:w-[320px] h-[200px] overflow-hidden rounded-lg shrink-0">
               <img
-                src={`${NewsImage_Url}${item.image.replace("/temp/", "/news/")}`}
+                src={getImageUrl(item.image)}
                 alt={item.heading}
                 className="w-full h-full object-cover group-hover:scale-110 transition duration-500"
               />
